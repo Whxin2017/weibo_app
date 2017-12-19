785c86b5a276ca51a126c5b7ce4c9df16860157f
Whxin2017 <thmin1996@163.com>
2017-12-19 23:04:11 +0800

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	  @user = User.new(user_params)
 	  if @user.save
 		  #succeed
+		  log_in @user
 		  flash[:success] = "Welcome to the WEIBO APP!"
 		  redirect_to @user
 	  else
