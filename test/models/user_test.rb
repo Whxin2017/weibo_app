019e9e15a2f1eb0d94613a7dec05665374d793f5
Whxin2017 <thmin1996@163.com>
2018-01-18 10:54:36 +0800

    update user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,15 +5,14 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 	def setup
-		@user=User.new(name:"Example User",email:"user@example.com",password:"onepiece",password_confirmation:"onepiece")
+		@user=User.new(name:"Example User",email:"user@example.com",
+					   password:"onepiece",password_confirmation:"onepiece")
 	end
 
 	test "should be valid" do
 		assert @user.valid?
 	end
-	
-	
-	
+		
 	test "name should be present" do
 		@user.name = "    "
 		assert_not @user.valid?
